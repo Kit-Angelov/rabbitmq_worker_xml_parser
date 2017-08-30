fc2ebad1ea0d980d81311bd6d5126c9995563f2e
DESKTOP-OHL2LMT\user <kit.angelov@gmail.com>
2017-08-30 18:53:39 +0300

    add kpt10 0.2

--- worker_parser_xml/config.py
+++ worker_parser_xml/config.py
@@ -1,13 +1,17 @@
-from xml_parse_project.worker_parser_xml.XML_HANDLERS.xml_handlers import HandlerKVZU7, HandlerKPZU6
+from xml_parse_project.worker_parser_xml.XML_HANDLERS.xml_handlers import HandlerKVZU7, HandlerKPZU6, HandlerKPT10
 
 from xml_parse_project.worker_parser_xml.type_and_code_checker import CodeChecker
 
-type_list = {'KPT': '...',
-             'KVZU': CodeChecker}
+type_list = {
+    'KPT': CodeChecker,
+    'KVZU': CodeChecker,
+    'KPZU': CodeChecker,
+}
 
 handlers_dict = {
     'KVZU': {"urn://x-artefacts-rosreestr-ru/outgoing/kvzu/7.0.1": HandlerKVZU7},
     'KPZU': {"urn://x-artefacts-rosreestr-ru/outgoing/kpzu/6.0.1": HandlerKPZU6},
+    'KPT': {"urn://x-artefacts-rosreestr-ru/outgoing/kpt/10.0.1": HandlerKPT10},
 }
 
 feature_tags = ['parcel', ]
